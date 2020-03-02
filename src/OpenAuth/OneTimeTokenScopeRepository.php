f0c3109338210b24093c44038d062c1a580dca8f
Joshua Behrens <behrens@heptacom.de>
2020-03-02 01:21:10 +0100

    Fix bug that write scope is missing with open auth login
    FOSS-11

---
+++ src/OpenAuth/OneTimeTokenScopeRepository.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\AdminOpenAuth\OpenAuth;
+
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Repositories\ScopeRepositoryInterface;
+
+class OneTimeTokenScopeRepository implements ScopeRepositoryInterface
+{
+    /**
+     * @var ScopeRepositoryInterface
+     */
+    private $decorated;
+
+    public function __construct(ScopeRepositoryInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function getScopeEntityByIdentifier($identifier)
+    {
+        return $this->decorated->getScopeEntityByIdentifier($identifier);
+    }
+
+    public function finalizeScopes(array $scopes, $grantType, ClientEntityInterface $clientEntity, $userIdentifier = null)
+    {
+        if ($grantType === 'heptacom_admin_open_auth_one_time_token') {
+            $grantType = 'password';
+        }
+
+        return $this->decorated->finalizeScopes($scopes, $grantType, $clientEntity);
+    }
+}
