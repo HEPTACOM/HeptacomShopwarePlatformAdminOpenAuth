f70998fa983bbc3c71d3531661afdad61bdde7e7
Joshua Behrens <behrens@heptacom.de>
2020-03-04 01:46:46 +0100

    Add provider specific client editing
    FOSS-11

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,12 +1,16 @@
 import './extension/sw-profile-index';
 import './module/heptacom-admin-open-auth-client';
+import './provider/jira/heptacom-admin-open-auth-client-edit-page';
+import './provider/microsoft_azure/heptacom-admin-open-auth-client-edit-page';
 import './init/services.init';
 import globalSnippets from './snippets';
 import extensionSnippets from './extension/snippets';
+import providerJiraSnippets from './provider/jira/snippets';
+import providerMicrosoftAzureSnippets from './provider/microsoft_azure/snippets';
 
 const { Locale } = Shopware;
 
-[globalSnippets, extensionSnippets]
+[globalSnippets, extensionSnippets, providerJiraSnippets, providerMicrosoftAzureSnippets]
     .map(Object.entries)
     .flat()
     .forEach(([lang, snippets]) => Locale.extend(lang, snippets));
---
+++ src/Resources/app/administration/src/provider/microsoft_azure/heptacom-admin-open-auth-client-edit-page/index.js
@@ -0,0 +1,7 @@
+import template from './heptacom-admin-open-auth-client-edit-page.html.twig';
+
+const { Component } = Shopware;
+
+Component.override('heptacom-admin-open-auth-client-edit-page', {
+    template
+});
