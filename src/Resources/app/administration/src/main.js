8a80abb4b21692d81f811f396e05f2ca5d6e7305
Joshua Behrens <behrens@heptacom.de>
2020-10-11 13:57:06 +0200

    Remove duplicate settings item when shopware version >=6.2
    FOSS-11

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,6 +1,5 @@
 import './app/components/heptacom-admin-open-auth-scope-field';
 import './extension/sw-profile-index';
-import './extension/sw-settings-index';
 import './module/heptacom-admin-open-auth-client';
 import './provider/jira/heptacom-admin-open-auth-client-edit-page';
 import './provider/microsoft_azure/heptacom-admin-open-auth-client-edit-page';
