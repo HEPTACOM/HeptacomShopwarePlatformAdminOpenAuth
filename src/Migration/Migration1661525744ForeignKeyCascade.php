48753f72820d0cfcd11c7f61db60982b195a5ace
bitbucket-pipelines <commits-noreply@bitbucket.org>
2022-08-26 15:14:54 +0000

    $ make cs-fix-ecs

--- src/Migration/Migration1661525744ForeignKeyCascade.php
+++ src/Migration/Migration1661525744ForeignKeyCascade.php
@@ -20,41 +20,41 @@ class Migration1661525744ForeignKeyCascade extends MigrationStep
 ALTER TABLE `heptacom_admin_open_auth_login`
 DROP FOREIGN KEY `fk.heptacom_admin_open_auth_login.user_id`,
 DROP INDEX `fk.heptacom_admin_open_auth_login.user_id`
-SQL );
+SQL);
         $connection->executeStatement(<<<SQL
 ALTER TABLE `heptacom_admin_open_auth_login`
 ADD CONSTRAINT `fk.heptacom_admin_open_auth_login.user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-SQL );
+SQL);
 
         $connection->executeStatement(<<<SQL
 ALTER TABLE `heptacom_admin_open_auth_user_email`
 DROP FOREIGN KEY `fk.heptacom_admin_open_auth_user_email.user_id`,
 DROP INDEX `fk.heptacom_admin_open_auth_user_email.user_id`
-SQL );
+SQL);
         $connection->executeStatement(<<<SQL
 ALTER TABLE `heptacom_admin_open_auth_user_email`
 ADD CONSTRAINT `fk.heptacom_admin_open_auth_user_email.user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-SQL );
+SQL);
 
         $connection->executeStatement(<<<SQL
 ALTER TABLE `heptacom_admin_open_auth_user_key`
 DROP FOREIGN KEY `fk.heptacom_admin_open_auth_user_key.user_id`,
 DROP INDEX `fk.heptacom_admin_open_auth_user_key.user_id`
-SQL );
+SQL);
         $connection->executeStatement(<<<SQL
 ALTER TABLE `heptacom_admin_open_auth_user_key`
 ADD CONSTRAINT `fk.heptacom_admin_open_auth_user_key.user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-SQL );
+SQL);
 
         $connection->executeStatement(<<<SQL
 ALTER TABLE `heptacom_admin_open_auth_user_token`
 DROP FOREIGN KEY `fk.heptacom_admin_open_auth_user_token.user_id`,
 DROP INDEX `fk.heptacom_admin_open_auth_user_token.user_id`
-SQL );
+SQL);
         $connection->executeStatement(<<<SQL
 ALTER TABLE `heptacom_admin_open_auth_user_token`
 ADD CONSTRAINT `fk.heptacom_admin_open_auth_user_token.user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-SQL );
+SQL);
     }
 
     public function updateDestructive(Connection $connection): void
