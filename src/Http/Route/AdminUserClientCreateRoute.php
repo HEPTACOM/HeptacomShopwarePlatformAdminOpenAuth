1720e0f0c294a352f63095463399a59166c6d5be
Joshua Behrens <behrens@heptacom.de>
2023-05-15 01:11:33 +0200

    Extract admin user client create route
    FOSS-33

--- src/Http/Route/AdminUserClientCreateRoute.php
+++ src/Http/Route/AdminUserClientCreateRoute.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Heptacom\AdminOpenAuth\Controller;
+namespace Heptacom\AdminOpenAuth\Http\Route;
 
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Heptacom\AdminOpenAuth\Contract\ClientLoaderInterface;
@@ -10,42 +10,43 @@ use Heptacom\AdminOpenAuth\Database\ClientDefinition;
 use Shopware\Core\Framework\Api\Response\ResponseFactoryInterface;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @RouteScope(scopes={"administration"})
- */
-final class AdministrationController extends AbstractController
+final class AdminUserClientCreateRoute extends AbstractController
 {
     public function __construct(
         private readonly ClientLoaderInterface $clientLoader,
+        private readonly ClientDefinition $definition,
+        private readonly EntityRepository $clientsRepository,
     ) {
     }
 
-    /**
-     * @Route(
-     *     methods={"POST"},
-     *     name="api.heptacom.admin_open_auth.provider.factorize",
-     *     path="/api/_action/heptacom_admin_open_auth_provider/factorize"
-     * )
-     */
-    public function createClient(
-        Request $request,
-        ResponseFactoryInterface $responseFactory,
-        ClientDefinition $definition,
-        EntityRepository $clientsRepository,
-        Context $context
-    ): Response {
+    #[Route(
+        path: '/api/_action/heptacom_admin_open_auth_provider/factorize',
+        name: 'api.heptacom.admin_open_auth.provider.factorize',
+        defaults: [
+            '_routeScope' => ['administration']
+        ],
+        methods: ['POST']
+    )]
+    public function createClient(Request $request, ResponseFactoryInterface $responseFactory, Context $context): Response
+    {
         $providerKey = $request->get('provider_key');
         $clientId = $this->clientLoader->create($providerKey, $context);
         $criteria = new Criteria();
         $criteria->setIds([$clientId]);
-        $entity = $clientsRepository->search($criteria, $context)->first();
+        $entity = $this->clientsRepository->search($criteria, $context)->first();
 
-        return $responseFactory->createDetailResponse($criteria, $entity, $definition, $request, $context, false);
+        return $responseFactory->createDetailResponse(
+            $criteria,
+            $entity,
+            $this->definition,
+            $request,
+            $context,
+            false
+        );
     }
 }
