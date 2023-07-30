4ebd0025b84e0ad69c36780f36976f9670f9d0ed
Joshua Behrens <behrens@heptacom.de>
2023-07-30 17:19:41 +0200

    Migrate FactorizeClientProviderNotFoundException from OpenAuth package

--- src/Contract/Client/ClientFactoryContract.php
+++ src/Contract/Client/ClientFactoryContract.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Heptacom\AdminOpenAuth\Contract\Client;
 
+use Heptacom\AdminOpenAuth\Contract\Client\Exception\FactorizeClientProviderNotFoundException;
 use Heptacom\AdminOpenAuth\Contract\ClientProvider\ClientProviderContract;
 use Heptacom\AdminOpenAuth\Contract\ClientProvider\ClientProviderRepositoryContract;
 use Heptacom\AdminOpenAuth\Contract\ClientProvider\Exception\ProvideClientException;
 use Heptacom\OpenAuth\Client\Exception\FactorizeClientInvalidConfigurationException;
 use Heptacom\OpenAuth\Client\Exception\FactorizeClientProviderFailedException;
-use Heptacom\OpenAuth\Client\Exception\FactorizeClientProviderNotFoundException;
 
 class ClientFactoryContract
 {
---
+++ src/Contract/Client/Exception/FactorizeClientProviderNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\AdminOpenAuth\Contract\Client\Exception;
+
+use Heptacom\OpenAuth\Client\Exception\FactorizeClientException;
+
+final class FactorizeClientProviderNotFoundException extends FactorizeClientException
+{
+}
