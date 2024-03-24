a7a76d8d8f99aea860daf484ae051d6444d32492
Joshua Behrens <behrens@heptacom.de>
2024-03-24 20:50:57 +0100

    Add check to ensure the plugin initialization script is used

--- src/HeptacomShopwarePlatformPluginName.php
+++ src/HeptacomShopwarePlatformPluginName.php
@@ -9,3 +9,6 @@ use Shopware\Core\Framework\Plugin;
 class HeptacomShopwarePlatformPluginName extends Plugin
 {
 }
+# BEGIN-INIT-PLUGIN
+die("Run dev-ops/init-plugin.php or in bitbucket the custom pipeline 'init-plugin' to ensure the branding of the plugin to make this plugin usable: " . __FILE__);
+# END-INIT-PLUGIN
