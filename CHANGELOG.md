d526f35a24bdcc0dded270fbdcc38e040aa01d49
Joshua Behrens <behrens@heptacom.de>
2026-04-09 02:10:00 +0200

    implement unified redirect domain
    FOSS-13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Unreleased
 
+**Added**
+
+* Added configuration options `enableUnifiedRedirectDomain` and `unifiedRedirectDomain` to allow using one unified domain for the IdP redirect to the shop. (copied from 8.1.0)
+
 # 7.0.2
 
 **Changed**
--- src/Service/RedirectBehaviourFactoryService.php
+++ src/Service/RedirectBehaviourFactoryService.php
@@ -8,15 +8,20 @@ use Heptacom\AdminOpenAuth\Contract\ClientLoaderInterface;
 use Heptacom\AdminOpenAuth\Contract\ModifiedRedirectBehaviourClientContract;
 use Heptacom\AdminOpenAuth\Contract\RedirectBehaviour;
 use Heptacom\AdminOpenAuth\Contract\RedirectBehaviourFactoryInterface;
+use Heptacom\AdminOpenAuth\KskHeptacomAdminOpenAuth;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouterInterface;
 
 final readonly class RedirectBehaviourFactoryService implements RedirectBehaviourFactoryInterface
 {
     public function __construct(
+        private SystemConfigService $systemConfigService,
         private ClientLoaderInterface $clientLoader,
-        private RouterInterface $router
+        private RouterInterface $router,
+        private string $appUrl,
     ) {
     }
 
@@ -24,15 +29,38 @@ final readonly class RedirectBehaviourFactoryService implements RedirectBehaviou
     {
         $client = $this->clientLoader->load($clientId, $context);
 
+        $requestContext = $this->router->getContext();
+        if ($this->systemConfigService->getBool(KskHeptacomAdminOpenAuth::CONFIG_ENABLE_UNIFIED_REDIRECT_DOMAIN)) {
+            $modifiedContext = clone $requestContext;
+
+            $unifiedDomain = $this->systemConfigService->getString(KskHeptacomAdminOpenAuth::CONFIG_UNIFIED_REDIRECT_DOMAIN);
+            if ($unifiedDomain === '') {
+                $unifiedDomain = $this->appUrl;
+            }
+
+            $unifiedDomainContext = RequestContext::fromUri($unifiedDomain);
+
+            $modifiedContext->setHost($unifiedDomainContext->getHost());
+            $modifiedContext->setScheme($unifiedDomainContext->getScheme());
+
+            if ($modifiedContext->isSecure()) {
+                $modifiedContext->setHttpsPort($unifiedDomainContext->getHttpsPort());
+            } else {
+                $modifiedContext->setHttpPort($unifiedDomainContext->getHttpPort());
+            }
+
+            $this->router->setContext($modifiedContext);
+        }
+
         $behaviour = new RedirectBehaviour();
         $behaviour->expectState = true;
         $behaviour->redirectUri = $this->router->generate(
             'administration.heptacom.admin_open_auth.login',
-            [
-                'clientId' => $clientId,
-            ],
+            [ 'clientId' => $clientId ],
             UrlGeneratorInterface::ABSOLUTE_URL
         );
+
+        $this->router->setContext($requestContext);
 
         if ($client instanceof ModifiedRedirectBehaviourClientContract) {
             $client->modifyRedirectBehaviour($behaviour);
